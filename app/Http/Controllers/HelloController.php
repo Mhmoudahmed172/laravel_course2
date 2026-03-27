ed6378a325e67198ea088d657378c8727d8583e8
Mhmoudahmed172 <125897461+Mhmoudahmed172@users.noreply.github.com>
2026-03-27 12:47:43 +0300

    up

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -8,13 +8,11 @@ use Illuminate\Http\Request;
 class HelloController extends Controller
 {
     public function welcome ($name=null) {
-//        dd($name ?? 'there is nothing here');
-
-//        if (is_null($name)) {
-//            dd('there is nothing here');
-//        } else {
-//            dd($name);
-//        }
+        if (is_null($name)) {
+            dd('there is nothing here');
+        } else {
+            dd($name);
+        }
     }
 
     public function showText ($first_name=null) {
